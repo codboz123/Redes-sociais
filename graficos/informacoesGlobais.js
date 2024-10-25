18e6a479a585434971adcedda8d8ec8db310a459
codboz123 <106533407+codboz123@users.noreply.github.com>
2024-10-25 10:56:23 +0000

    .

---
+++ graficos/informacoesGlobais.js
@@ -0,0 +1,6 @@
+const url='https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+async function visualizar InformaçõesGlobais() {
+    const res = await fetch(url);
+    const dados = await res.json();
+    console.log(dados);
+}
