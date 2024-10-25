8fcee4080f320bfe0f9332d7266c863f45edb3db
codboz123 <106533407+codboz123@users.noreply.github.com>
2024-10-25 11:12:40 +0000

    .

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -4,4 +4,7 @@ async function visualizar InformaçõesGlobais() {
     const dados = await res.json();
     console.log(dados);
     const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container_texto')
+
+    paragrafo.innerHTML = 'Você sabia que mundo tem <span>${dados.total_pessoas_mundo}</span> de pessoas e que aproximadamente '
 }
